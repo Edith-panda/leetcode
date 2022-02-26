478c1a4414cbde5d2c1a5794306c7d183b8a2753
Sakure_Yashasvi_0566 <yashasvisakure@gmail.com>
2022-02-26 16:22:48 +0530

    Time: 4 ms (40.12%), Space: 5.9 MB (48.03%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int i=32;
+        int count =0;
+        while(i >0){
+            if((n&1) ==1){
+                count++;
+            }
+            n= n >> 1;
+            i--;
+}
+        return count;
+    }
+};
