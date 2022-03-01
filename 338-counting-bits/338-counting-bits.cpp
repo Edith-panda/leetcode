76a8cd10350307bcdfe4bddf10c424dfaa31eb8d
Sakure_Yashasvi_0566 <yashasvisakure@gmail.com>
2022-03-01 08:57:24 +0530

    Time: 7 ms (62.32%), Space: 7.9 MB (75.07%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int binary(int a){
+        int sol = 0;
+        while(a >0){
+            int p = a%2;
+            sol += p;
+            a = a/2;
+        }
+        return sol;
+    }
+    vector<int> countBits(int n) {
+        vector <int> v(n+1);
+        for(int i=1;i<=n;i++){
+            v[i] = binary(i);
+        }
+        return v;
+    }
+};
