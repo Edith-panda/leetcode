ad203ec06b486326b0134f37ef84a9f8a9ed5b46
Sakure_Yashasvi_0566 <yashasvisakure@gmail.com>
2022-03-31 11:29:39 +0530

    Time: 48 ms (58.51%), Space: 27.6 MB (12.69%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -4,17 +4,16 @@ public:
         int n = nums.size();
         int i=0, j = n-1;
         while(i <= j){
-            int mid = i + (j-i)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                i = mid +1;
+           int mid = i + (j-i)/2;
+            if(nums[mid] < target){
+                i = mid+1;
             }
             else if(nums[mid] > target){
-                j = mid -1;
+                j = mid-1;
             }
-            
+            else if(nums[mid] == target){
+                return mid;
+            }
         }
         return -1;
     }
