f522971953a25db2b4ad7f9eed667c8af0bca75e
Sakure_Yashasvi_0566 <yashasvisakure@gmail.com>
2022-04-06 21:29:13 +0530

    Time: 1139 ms (24.15%), Space: 26.7 MB (5.44%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size();
+        long long int mod=1e9+7;
+        int c =0;
+        for(int i=0;i<n;i++){
+            vector<int> count(101, 0);
+            for(int j=i+1;j<n;j++){
+                int k = target - (arr[i] + arr[j]);
+                    if(k >=0 && k <=100 && count[k] > 0){
+                        c += count[k];
+                        c %= mod;
+                    }
+                count[arr[j]]++;
+                }
+            }
+        
+        return (int)c;
+    }
+};
